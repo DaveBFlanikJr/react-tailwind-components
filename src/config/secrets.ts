e19df37f0da1250fe8c2dd6ffe98a769ebae36fb
David Flanik <Daveflanik@gmail.com>
2023-11-02 23:24:27 +0900

    getting ready for RAWG API

---
+++ src/config/secrets.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+const envFound = dotenv.config();
+
+if (envFound.error) {
+  throw new Error(".env folder not found");
+}
+
+const API_KEY = process.env.API_KEY;
+console.log(API_KEY);
+
+// export default API_KEY;
