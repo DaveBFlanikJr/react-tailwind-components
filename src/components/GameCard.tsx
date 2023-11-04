524339a6bcdb86c1b85e82e4bf2fe89121d4a31d
David Flanik <Daveflanik@gmail.com>
2023-11-04 18:21:11 +0900

    working on product card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,17 +1,22 @@
+import gameImage from "/game.jpg";
+
 const dummyData = [
   {
+    image: gameImage,
     title: "Gears of War",
     description: "Fantasy RPG based on old greek theology",
     released: 2018,
     rating: 4,
   },
   {
+    image: gameImage,
     title: "Halo",
     description: "Fantasy FPS set in the future",
     released: 2007,
     rating: 4.5,
   },
   {
+    image: gameImage,
     title: "Pokemon",
     description: "Fantasy game based on a popular Japanese manga",
     released: 1999,
@@ -21,17 +26,22 @@ const dummyData = [
 
 export default function GameCard() {
   return (
-    <div>
-      <ul>
+    <article className="justify-center items-center">
+      <ul key="game-list">
         {dummyData.map((game) => (
-          <div key={game.title}>
-            <li>{game.title}</li>
+          <li key={game.title} className="mb-2">
+            <img
+              src={gameImage}
+              alt={game.title}
+              className="w-1/4 h-1/4 rounded-full"
+            />
+            <h3 className="text-xl font-bold">{game.title}</h3>
             <p>{game.description}</p>
-            <li>{game.released}</li>
-            <li>{game.rating}</li>
-          </div>
+            <span className="text-gray-700">Released: {game.released}</span>
+            <span className="text-gray-700">Rating: {game.rating}</span>
+          </li>
         ))}
       </ul>
-    </div>
+    </article>
   );
 }
