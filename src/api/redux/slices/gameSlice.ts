4f442aac3070f61b4ef525b260e5af4e755c9fff
David Flanik <Daveflanik@gmail.com>
2023-11-04 17:10:56 +0900

    creating qeury endpoints for games..Needs more work

--- src/api/redux/slices/gameSlice.ts
+++ src/api/redux/slices/gameSlice.ts
@@ -1,1 +1,20 @@
 // Will hold the redux toolkit logic to manage the state for individual games
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createSlice } from "@reduxjs/toolkit";
+import API_KEY from "../../../config/secrets";
+
+const baseUrl: string = `https://api.rawg.io/api/games`;
+const gamesAPIHeader = { "content-type": "application/json" };
+const createRequest = (url: string) => ({ url, headers: gamesAPIHeader });
+
+const gameSlice = createSlice({
+  name: "gamesAPI",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => {
+    getGame: builder.query({
+      query: (id: number) => createRequest(`games/${id}?key=${API_KEY}`),
+    });
+  },
+});
+
+export default gameSlice;
--- src/api/redux/store.ts
+++ src/api/redux/store.ts
@@ -1,1 +1,8 @@
 // Will contain the redux toolkit logic to manage all the state of the data used in this application
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {},
+});
+
+export default store;
