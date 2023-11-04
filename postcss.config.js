cadf442cfc04b304a31c51a372f4aa1bd86e483b
David Flanik <Daveflanik@gmail.com>
2023-11-04 17:07:26 +0900

    needed to change the export syntax

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,12 @@
-export default {
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
+
+// eslint-disable-next-line no-undef
+module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
